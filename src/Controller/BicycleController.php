154ef76132cab3d27ddff2b902ceeda24350574c
carrollm22 <carrollm22@hanover.edu>
2023-07-26 19:25:00 -0400

    Add actions for bicycle functions

--- src/Controller/BicycleController.php
+++ src/Controller/BicycleController.php
@@ -4,13 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Bicycle;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
 class BicycleController extends AbstractController
 {
-    #[Route('/main', name: 'main')]
+    /**
+     * @param EntityManagerInterface $entityManager
+     *
+     * @return Response
+     * @Route("/main", name="main")
+     */
     public function main(EntityManagerInterface $entityManager): Response
     {
         $bicycleRepository = $entityManager->getRepository('Bicycle');
@@ -20,10 +26,91 @@ class BicycleController extends AbstractController
             $bicycle = new Bicycle();
         }
 
+        $entityManager->persist($bicycle);
+        $entityManager->flush();
+
         return $this->render('templates/base.html.twig', [
             'bicycle' => $bicycle,
         ]);
     }
 
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param string $id
+     *
+     * @return RedirectResponse
+     * @Route("/ride/{id}", requirements={"id" = "\d+"}, name="ride")
+     */
+    public function ride(EntityManagerInterface $entityManager, string $id): Response
+    {
+        $bicycleRepository = $entityManager->getRepository('Bicycle');
+        $bicycle = $bicycleRepository->findoneBy(['id' => $id]);
 
+        $bicycle = $bicycle->ride();
+
+        $entityManager->persist($bicycle);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main');
+    }
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param string $id
+     *
+     * @return RedirectResponse
+     * @Route("/shiftUp", requirements={"id" = "\d+"}, name="shiftUp")
+     */
+    public function shiftUp(EntityManagerInterface $entityManager, string $id): Response
+    {
+        $bicycleRepository = $entityManager->getRepository('Bicycle');
+        $bicycle = $bicycleRepository->findoneBy(['id' => $id]);
+
+        $bicycle = $bicycle->shiftUp();
+
+        $entityManager->persist($bicycle);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main');
+    }
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param string $id
+     *
+     * @return RedirectResponse
+     * @Route("/shiftDown", requirements={"id" = "\d+"}, name="shiftDown")
+     */
+    public function shiftDown(EntityManagerInterface $entityManager, string $id): Response
+    {
+        $bicycleRepository = $entityManager->getRepository('Bicycle');
+        $bicycle = $bicycleRepository->findoneBy(['id' => $id]);
+
+        $bicycle = $bicycle->shiftDown();
+
+        $entityManager->persist($bicycle);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main');
+    }
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param string $id
+     *
+     * @return RedirectResponse
+     * @Route("/inflateTires", requirements={"id" = "\d+"}, name="inflateTires")
+     */
+    public function inflateTires(EntityManagerInterface $entityManager, string $id): Response
+    {
+        $bicycleRepository = $entityManager->getRepository('Bicycle');
+        $bicycle = $bicycleRepository->findoneBy(['id' => $id]);
+
+        $bicycle = $bicycle->inflateTires();
+
+        $entityManager->persist($bicycle);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main');
+    }
 }
